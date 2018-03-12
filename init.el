6700424870405597de4d78964c3875cf0a4ee1bc
Grey Zhang <greyzhang@126.com>
2018-03-12 23:54:51 +0800

    1. Add evil and enable evil-mode. 2. Add package goto-chg which is needed by evil. 3. Add package undo-tree which is needed by evil.

--- init.el
+++ init.el
@@ -1,1 +1,28 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq make-backup-files nil)
+(global-linum-mode t)
+(global-hl-line-mode t)
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+(global-undo-tree-mode)
+(add-to-list 'load-path "~/.emacs.d/packages/goto-chg")
+(require 'goto-chg)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (undo-tree))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
