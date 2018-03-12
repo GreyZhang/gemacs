161ff3b19ec48ef6c5c8ad6f50011a910c048cd8
Grey Zhang <greyzhang@126.com>
2018-03-13 00:10:08 +0800

    1. Add company and enable company-mode; 2. Add monokai theme and use it as default; 3. Change package source to emacs-china; 4. Add smartparents. 5. Disable bell warings.

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
-
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -14,12 +15,26 @@
 (global-undo-tree-mode)
 (add-to-list 'load-path "~/.emacs.d/packages/goto-chg")
 (require 'goto-chg)
+(setq visible-bell 0)
+(global-company-mode t)
+(require 'smartparens-config)
+(smartparens-global-mode t)
+(load-theme 'monokai t)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (undo-tree))))
+ '(company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-xcode company-cmake company-capf company-files
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-dabbrev)))
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 1)
+ '(package-selected-packages (quote (monokai-theme smartparens company undo-tree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
