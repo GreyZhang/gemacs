400a7edfbfa42a6d4d0ab1ba548a28f824e54dd5
greyzhang <greyzhang@126.com>
2018-03-14 11:33:32 +0800

    New organization for a better management about init.el

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;;********************************************************************************
+;; melpa source configurations
 (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
                          ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 ;; Added by Package.el.  This must come before configurations of
@@ -6,25 +8,46 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;;********************************************************************************
+;; Common emacs configurations
 (setq make-backup-files nil)
 (global-linum-mode t)
 (global-hl-line-mode t)
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-(global-undo-tree-mode)
-(add-to-list 'load-path "~/.emacs.d/packages/goto-chg")
-(require 'goto-chg)
 (setq visible-bell 0)
-(global-company-mode t)
-(require 'smartparens-config)
-(smartparens-global-mode t)
-(load-theme 'monokai t)
 (setq inhibit-startup-screen t)
 (setq cursor-in-non-selected-windows nil)
 (setq delete-old-versions t)
 (show-smartparens-global-mode t)
 (scroll-bar-mode 0)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(defun open-my-emacs-init-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f1>") 'open-my-emacs-init-file)
+;;********************************************************************************
+;; Evil configurations
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+;;********************************************************************************
+;; undo-tree configurations
+(global-undo-tree-mode)
+(add-to-list 'load-path "~/.emacs.d/packages/goto-chg")
+;;********************************************************************************
+;; goto-chg configurations
+(require 'goto-chg)
+;;********************************************************************************
+;; company configurations
+(global-company-mode t)
+;;********************************************************************************
+;; smartparens configurations
+(require 'smartparens-config)
+(smartparens-global-mode t)
+;;********************************************************************************
+;; theme selection
+(load-theme 'monokai t)
+;;********************************************************************************
+;; C/C++ language configurations
 ;; Use TAB itself as indent method in C/C++
 (global-set-key (kbd "TAB") 'self-insert-command)
 (setq default-tab-width 4)
@@ -34,22 +57,25 @@
 	c-indent-level 4
 	c-default-style "bsd"))
 (add-hook 'c-mode-common-hook 'my-c-mode-hook)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(require 'smex) ; Not needed if you use package.el
-(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+;;********************************************************************************
+;; smex configurations
+;;(require 'smex) ; Not needed if you use package.el
+;;(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
                     ; when Smex is auto-initialized on its first run.
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;;(global-set-key (kbd "M-x") 'smex)
+;;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(defun open-my-emacs-init-file ()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-(global-set-key (kbd "<f1>") 'open-my-emacs-init-file)
+;;(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;;********************************************************************************
+;; helm configurations
 (require 'helm-config)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
 
+
+
+;;********************************************************************************
+;; Configurations generated automatically.
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
