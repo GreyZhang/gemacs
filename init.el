9b22ce841f5771bff401e48d51d814fa463dc78d
GreyZhang <greyzhang@126.com>
2018-04-10 14:03:04 +0800

    Configure font which would have a better effect on Mac.

--- init.el
+++ init.el
@@ -156,4 +156,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))
+ '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
