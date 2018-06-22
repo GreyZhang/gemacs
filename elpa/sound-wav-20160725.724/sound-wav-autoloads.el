dfc7db924a3cfef6e6e1252c5b3cb8b305315f76
Grey Zhang <greyzhang@126.com>
2018-06-22 13:38:18 +0800

    remove some files not need to keep in git.

--- elpa/sound-wav-20160725.724/sound-wav-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; sound-wav-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "sound-wav" "sound-wav.el" (23225 4011 0 0))
-;;; Generated autoloads from sound-wav.el
-
-(autoload 'sound-wav-play "sound-wav" "\
-
-
-\(fn &rest FILES)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sound-wav-autoloads.el ends here
