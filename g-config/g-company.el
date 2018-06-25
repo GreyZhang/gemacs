4ee28fb7c76b533aed42e1099a38a301bc6b90ec
Grey Zhang <greyzhang@126.com>
2018-06-25 13:39:31 +0800

    1. Use C-j and C-k to select company items which is also used in Spacemacs. 2. Update the font display.

--- g-config/g-company.el
+++ g-config/g-company.el
@@ -1,2 +1,8 @@
 (global-company-mode 1)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-j") #'company-select-next)
+  (define-key company-active-map (kbd "C-k") #'company-select-previous))
+
 (provide 'g-company)
